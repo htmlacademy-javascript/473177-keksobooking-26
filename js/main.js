6c931cdc716983a6f57e49efcf962104f40c20b5
Анастасия Колмакова <nastya.miss.7@gmail.com>
2022-06-27 01:23:29 +0300

    Делает реализацию ссылок аватаров рабочей и добавляет переменные для широты и долготы

--- js/main.js
+++ js/main.js
@@ -32,32 +32,45 @@ function getRandomPositiveInteger (a, b) {
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-};
+}
 
 function getRandomPositiveFloat (a, b, digits = 5) {
   const lower = Math.min(Math.abs(a), Math.abs(b));
   const upper = Math.max(Math.abs(a), Math.abs(b));
   const result = Math.random() * (upper - lower) + lower;
   return +result.toFixed(digits);
+}
+
+const createImageLinks = (maxCountImage) => {
+  const imageLinks = [];
+  for (let i = 1; i <= maxCountImage; i++) {
+    if (i < 10) {
+      imageLinks[i-1] = `img/avatars/user0${  i  }.png`;
+    } else {
+      imageLinks[i-1] = `img/avatars/user${  i  }.png`;
+    }
+  }
+  return imageLinks;
 };
 
-const createImagesLink = (maxCountImage) => {
-  for (i = 1; i <= maxCountImage; i++) {
-    if (i < 10) {
-      return 'img/avatars/user0' + i + '.png';
-    }
-    return 'img/avatars/user' + i + '.png';
-  };
+let indexImageLink = 0;
+const getImageLink = () => {
+  const imageLinks = createImageLinks(COUNT_OBJECT);
+  indexImageLink++;
+  return imageLinks[indexImageLink-1];
 };
 
 const createObject = () => {
+  const LOCATION_LAT = getRandomPositiveFloat(35.65, 35.7);
+  const LOCATION_LNG = getRandomPositiveFloat(139.7, 139.8);
+
   return {
     author: {
-      avatar: createImagesLink(COUNT_OBJECT),
+      avatar: getImageLink(),
     },
     offer: {
       title: TITLE,
-      address: location.lat + ',' + location.lng,
+      address: `${LOCATION_LAT},${LOCATION_LNG}`,
       price: getRandomPositiveInteger(0, 100000),
       type: TYPES[getRandomPositiveInteger(0, TYPES.length-1)],
       rooms: getRandomPositiveInteger(1, 4),
@@ -69,10 +82,10 @@ const createObject = () => {
       photos: PHOTOS.slice(0, getRandomPositiveInteger(2, PHOTOS.length-1)),
     },
     location: {
-      lat: getRandomPositiveFloat(35.65, 35.7),
-      lng: getRandomPositiveFloat(139.7, 139.8),
+      lat: LOCATION_LAT,
+      lng: LOCATION_LNG,
     },
-  }
+  };
 };
 
 const OBJECTS = Array.from({length: COUNT_OBJECT}, createObject);
